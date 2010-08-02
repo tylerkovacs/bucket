fc794a313182f570e630e6b1be9c9d9d3a6426a1
Tyler Kovacs <tyler.kovacs@gmail.com>
2010-08-02 16:12:31 -0700

    dsl and yaml serialization code was accidentally reversed

--- lib/bucket/test/serialization/dsl.rb
+++ lib/bucket/test/serialization/dsl.rb
@@ -1,16 +1,34 @@
-require 'yaml'
-
 class Bucket
   class Test
     module Serialization
-      module Yaml
+      module Dsl
         def self.included(base)
           base.extend(ClassMethods)
         end
 
+        def to_dsl
+          dsl = [ "define_test #{name.inspect} do" ]
+          ATTRIBUTE_NAMES.each do |attribute_name|
+            dsl << "  #{attribute_name} #{serialize_attribute(attribute_name)}"
+          end
+          dsl << "end\n"
+
+          dsl.join("\n")
+        end
+
+        def serialize_attribute(name)
+          value = send(name)
+          case name
+          when :start_at, :end_at
+            value ? value.to_s.inspect : value.inspect
+          else
+            value.inspect
+          end
+        end
+
         module ClassMethods
-          def from_yaml(string)
-            YAML.load(string)
+          def from_dsl(string)
+            instance_eval(string)
           end
         end
       end
